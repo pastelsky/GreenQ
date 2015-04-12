aa7f600215c7a5821b5193146169dc5bb8045d34
shubhamsizzles <shubhamsizzles@gmail.com>
2015-04-12 07:56:01 +0530

    graphs

--- app/src/main/java/com/example/shubhamkanodia/gq/DisplayStats.java
+++ app/src/main/java/com/example/shubhamkanodia/gq/DisplayStats.java
@@ -1,9 +1,15 @@
 package com.example.shubhamkanodia.gq;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
+
 
 /**
  * Created by Chirag on 12-04-2015.
@@ -16,9 +22,42 @@ public class DisplayStats extends Activity {
         setContentView(R.layout.display_stats);
         Intent intent = getIntent();
         String[] myStrings = intent.getStringArrayExtra("strings");
+        getActionBar().setElevation(0);
 
-        TextView text = (TextView) findViewById(R.id.text);
+        getActionBar().setTitle("Results");
 
+        ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.cubiclinechart);
+
+        ValueLineSeries rspm = new ValueLineSeries();
+        rspm.setColor(0xFF56B7F1);
+
+        rspm.addPoint(new ValueLinePoint("Jan", 2.4f));
+        rspm.addPoint(new ValueLinePoint("Feb", 3.4f));
+        rspm.addPoint(new ValueLinePoint("Mar", .4f));
+
+
+        mCubicValueLineChart.addSeries(rspm);
+
+        ValueLineSeries no2 = new ValueLineSeries();
+        no2.setColor(0xFFB2B7F1);
+
+        no2.addPoint(new ValueLinePoint("Jan", 1.4f));
+        no2.addPoint(new ValueLinePoint("Feb", 2.4f));
+        no2.addPoint(new ValueLinePoint("Mar", 1.4f));
+
+
+        mCubicValueLineChart.addSeries(no2);
+
+        ValueLineSeries so2 = new ValueLineSeries();
+        so2.setColor(0xFFA2B7F7);
+
+        so2.addPoint(new ValueLinePoint("Jan", 1.4f));
+        so2.addPoint(new ValueLinePoint("Feb", 3.6f));
+        so2.addPoint(new ValueLinePoint("Mar", 2.4f));
+
+
+        mCubicValueLineChart.addSeries(so2);
+        mCubicValueLineChart.startAnimation();
 
     }
 }
